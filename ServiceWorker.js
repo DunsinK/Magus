839a30fbd58d114d07418ce49d5f08ac392c90f2
DunsinK <133840905+DunsinK@users.noreply.github.com>
2024-01-17 15:52:45 -0600

    Magus 0.2
    trying to make it work

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Magus-0.1";
+const contentToCache = [
+    "Build/Attempt 2.loader.js",
+    "Build/Attempt 2.framework.js.br",
+    "Build/Attempt 2.data.br",
+    "Build/Attempt 2.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
